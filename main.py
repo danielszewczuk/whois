32f2d7c8e8cf63461e0b8e7d48a3a1e9d2f1c770
Daniel Szewczuk <daniel@szewczuk.dev>
2024-02-22 14:05:27 +0100

    Added /api endpoint

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from api.query import *
 
 app = Flask(__name__)
+
+app.register_blueprint(api, url_prefix="/api")
+
 
 @app.route("/")
 def root():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
