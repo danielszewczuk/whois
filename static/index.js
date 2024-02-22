d1059e4c76f59b30669f111cdf0d6b940ab6fcc2
Daniel Szewczuk <daniel@szewczuk.dev>
2024-02-22 15:20:04 +0100

    Updated index.js

--- static/index.js
+++ static/index.js
@@ -1,18 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const submitButton = document.getElementById('submitButton');
-    const domainInput = document.getElementById('domainInput');
-    const resultContainer = document.getElementById('resultContainer');
+        const submitButton = document.getElementById('submitButton');
+        const domainInput = document.getElementById('domainInput');
+        const resultContainer = document.getElementById('resultContainer');
+    
+        submitButton.addEventListener('click', function() {
+            const domain = domainInput.value;
+    
+            const data = {
+                domain: domain
+            };
+
+            // Zamiast wysyłać żądanie do zewnętrznego serwera, wysyłamy je bezpośrednio do serwera
+            fetch('/api/query', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.text())
+            .then(data => {
+                const formattedData = data.replace(/\\r\\n/g, '<br>');
+                resultContainer.innerHTML = formattedData;
+            })
+            .catch(error => {
+                console.error('Wystąpił błąd:', error);
+                resultContainer.textContent = 'Wystąpił błąd podczas wysyłania żądania.';
+            });
+        });
+});
   
-    submitButton.addEventListener('click', function() {
-      const domain = domainInput.value;
-  
-      // Tworzymy obiekt zawierający dane do wysłania
-      const data = {
-        domain: domain
-      };
-  
-      // Wykonujemy żądanie POST
-      fetch('http://whoislookup.vercel.app/api/query', {
+
+      fetch('https://whoislookup.vercel.app/api/query', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -21,14 +40,11 @@ document.addEventListener('DOMContentLoaded', function() {
       })
       .then(response => response.text())
       .then(data => {
-        // Zamieniamy znaki \r\n na <br> w odpowiedzi
         const formattedData = data.replace(/\\r\\n/g, '<br>');
-        // Wyświetlamy wynik żądania POST
         resultContainer.innerHTML = formattedData;
       })
       .catch(error => {
         console.error('Wystąpił błąd:', error);
         resultContainer.textContent = 'Wystąpił błąd podczas wysyłania żądania.';
       });
-    });
-});
+
