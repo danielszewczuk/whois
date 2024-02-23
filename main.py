e1456f5d73ade536af702c7898ceb4e5d8ace32e
Daniel Szewczuk <daniel@szewczuk.dev>
2024-02-23 09:21:17 +0100

    Added 404 page

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from api.query import *
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(api, url_prefix="/api")
 
+@app.errorhandler(404)
+def notfound(error):
+    return render_template("404.html"), 404
+
 @app.route("/")
 def root():
     return render_template("index.html")
